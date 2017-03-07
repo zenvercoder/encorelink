535b734d57e320e97adb7cf88bab4dde1bcad915
ShannonB <tarislar@gmail.com>
2017-03-06 20:48:20 -0700

    Filthy hack added so we don't need APP_URL

--- common/models/user.js
+++ common/models/user.js
@@ -2,7 +2,31 @@ var mailerService = require('../../server/server-utils/emailProvider');
 
 module.exports = function(user) {
     var User = user;
+    var frontendUrl = "";
 
+    /* 
+    WARNING: Utterly disgusting, dirty hack.
+    So in order for our backend to find the correct URL to direct the client to in the email
+    when resetting their password, we need to access the 'req' object. From what I can tell
+    the only way to do that is with a pre or post hook, which is passed the 'context' argument as
+    the first one in the list.
+
+    I declard a variable above in a higher scope (frontendUrl) with an empty string. Before the
+    user.on 'resetpasswordrequest' is called, the beforeRemote method fires, gets the req object 
+    and sets frontendUrl equal to the headers.origin.
+
+    frontendUrl is then accessed by the 'resetPasswordRequest' to set the URL.
+
+    If anyone comes up with a better implementation, please go right ahead.
+
+    I'm sorry.
+    */
+
+
+    User.beforeRemote('**', function (ctx, unused, next) {
+      frontendUrl = ctx.req.headers.origin;
+      next();
+    });
 
     User.on('resetPasswordRequest', function(info){
       var accessToken = info.accessToken.id;
@@ -13,7 +37,7 @@ module.exports = function(user) {
         console.log("WARNING: No APP_URL defind. Cannot compose reset link in user.js");
       }
 
-      var resetURL = process.env.APP_URL + '/resetPassword/?id=' + user_id + '&token=' + accessToken;
+      var resetURL = frontendUrl + '/resetPassword/?id=' + user_id + '&token=' + accessToken;
    
       var emailObject = {
         to: email,
